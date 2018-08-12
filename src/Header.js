6ab91866fd803e6f61a7b0362fe9350588387dfe
Ramses Cabello <ramsescabello@Ramsess-MacBook.local>
2018-08-12 22:20:11 +0200

    First commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const appName = "Motsune";
+
+export class AppHeader extends React.Component {
+    render() {
+        return (
+            <h1>
+                { appName }
+            </h1>
+        );
+    };
+};
